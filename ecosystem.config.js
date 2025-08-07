d88d7e3bdb619a864c4030fa276eced1c18f14d6
Roberto Júnior <roberto.casali@CEREAIS.LOCAL>
2025-08-07 12:26:01 -0300

    configurações

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,18 @@
 module.exports = {
   apps : [{
     name   : "wms-zenith",
-    script : "server.js",
+    script : "./server.js",
+    instances: "max", // Executa uma instância por núcleo de CPU
+    exec_mode: "cluster", // Ativa o modo cluster para melhor performance e resiliência
+    autorestart: true, // Reinicia automaticamente se a aplicação falhar
+    watch: false, // Desativado, pois o build é manual
+    max_memory_restart: '1G', // Reinicia se a aplicação usar mais de 1GB de RAM
     env_production: {
-       "NODE_ENV": "production"
+      "NODE_ENV": "production",
+      "PORT": 3000
+    },
+    env_development: {
+      "NODE_ENV": "development"
     }
   }]
 };
