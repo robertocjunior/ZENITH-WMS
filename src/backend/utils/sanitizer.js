3d4daf08529b7e1fc471c5350922e25a945eb31d
Roberto.Junior <robertocasalijunior@gmail.com>
2025-09-17 10:26:26 -0300

    LICENSAS

--- src/backend/utils/sanitizer.js
+++ src/backend/utils/sanitizer.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) 2025 Roberto Casali Junior. Todos os Direitos Reservados.
+ *
+ * AVISO DE PROPRIEDADE E CONFIDENCIALIDADE
+ *
+ * Este código-fonte é propriedade intelectual confidencial e proprietária de
+ * Roberto Casali Junior. Seu uso, cópia, modificação, distribuição ou execução
+ * são estritamente proibidos sem a autorização prévia, expressa e por escrito
+ * do autor.
+ *
+ * Este software é regido pelos termos e condições estabelecidos no Contrato de
+ * Licença de Usuário Final (EULA) que o acompanha. A violação destes termos
+ * constitui uma infração à lei de direitos autorais (Lei nº 9.610/98) e
+ * sujeitará o infrator às sanções aplicáveis.
+ */
+
 // src/backend/utils/sanitizer.js
 
 const sanitizeStringForSql = (str) => {
